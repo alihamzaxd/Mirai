dc0fc76c9711f2cc8a66c0283d980277185e2b1f
Zihad Ahmed <zihad262940@gmail.com>
2025-03-18 22:38:11 +0600

    Update 21 Bot.js

--- scripts/commands/21 Bot.js
+++ scripts/commands/21 Bot.js
@@ -155,7 +155,7 @@ try{
 module.exports.handleEvent = async function ({ api, event }) {
 try{
     const body = event.body ? event.body.toLowerCase() : ""
-    if(body.startsWith("mim") || body.startsWith("hi") || body.startsWith("minu") || body.startsWith("bby") || body.startsWith("baby") || body.startsWith("মিম") || body.startsWith("janu")){
+    if(body.startsWith("mim") || body.startsWith("fariya") || body.startsWith("minu") || body.startsWith("bby") || body.startsWith("baby") || body.startsWith("মিম") || body.startsWith("janu")){
         const arr = body.replace(/^\S+\s*/, "")
       if(!arr) {
                                      await api.sendMessage("Yes 😀, i am here ", event.threadID, (error, info) => {
